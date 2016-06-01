b1fd8659c06ab243ef78d1ca4ea1ae540cbe7138
David Fudge <rkstar@mac.com>
2016-06-01 18:52:05 -0400

    updated spelling in README
    added the missing “i” from “multiply”

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rkstar:accounts-multiply',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'Support merging multiple login services to a single account in a Meteor app.',
   git: 'https://github.com/rkstar/accounts-multiply.git',
   documentation: 'README.md'
